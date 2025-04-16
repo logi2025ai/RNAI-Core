11135662ff0ba76a5e982beed548d2917468bff5
logi2025ai <carriertrafic@gmail.com>
2025-04-16 02:30:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-MY_SECRET_KEY = "mi_clave_ultra_secreta"
-
 @app.route("/")
 def home():
-    return f"¡Hola desde Revolution AI Crypto! Tu clave secreta es: {MY_SECRET_KEY}"
+    return "🚀 RNAI Server running with Flask + Railway!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8080)
